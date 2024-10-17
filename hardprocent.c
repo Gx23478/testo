55d20f6e3474bf831db3f5654911d8633284973e
Gx23478 <mezencevandrej35@gmail.com.com>
2024-10-17 23:04:32 +0300

    hardprocent2

--- hardprocent.c
+++ hardprocent.c
@@ -17,33 +17,75 @@ void ProcentDeposit(long double *deposit, int time, float percent, long double *
     
 }
 
+void SumProcentDeposit(long double *deposit, int time, float percent, long double *dopdeposit, long double *deposit1, long double *sumdeposit){ //вычиляет без реинвестирования с доп.вложениями
+    percent = 1 + percent / 100;
+    *deposit1 = *deposit;
+    for(int i = 0; i < time; i++){
+        *deposit = *deposit + *dopdeposit;
+        *deposit1 = *deposit1 + *dopdeposit;
+
+        *deposit = *deposit * percent;
+        *deposit = *deposit - (*deposit - *deposit1);
+    }
+    *sumdeposit = time * (*deposit - *deposit1);
+}
+
+void MaxSumDeposit(long double *deposit, int time, float percent, long double *dopdeposit){ //реинвестирование с доп.платежами
+    percent = 1 + percent / 100;
+    for(int i = 0; i <  time; i++){
+        *deposit = *deposit + *dopdeposit;
+        *deposit = *deposit * percent;
+    }
+}
+
 int main(){
-    long double deposit, deposit1, sumdeposit; 
+    long double deposit, deposit1, sumdeposit, dopdeposit; 
     int time;
     float percent;
     char answer[10];
 
     printf("Введите сумму вклада:");
     scanf("%Lf", &deposit);
+   
     printf("\nВведите процентную ставку:");
     scanf("%f", &percent);
+   
     printf("\nВведите продолжительность вклада:");
     scanf("%d", &time);
-    printf("Реинвестировать счёт?(да/нет)\n");
+  
+    printf("\nРеинвестировать счёт?(да/нет)\n");
     scanf("%s", answer);
+   
+    printf("\nСумма дополнительных вложений:");
+    scanf("%Lf", &dopdeposit);
     
-    if(strcmp(answer, "да") == 0){
+    
+    
+    if(strcmp(answer, "да") == 0){ //блок с реинвестированием
+        if(dopdeposit == 0){//реинвестирование без доп.вложений
         SumDeposit(&deposit, time, percent);
         
         printf("%.3Lf  ", deposit);
+        }
+        else{//реинвестирование с вложениями
+            MaxSumDeposit(&deposit, time, percent, &dopdeposit);
+
+            printf("Доход:%3.Lf", deposit);
+        }
     }
-    else{
+    else{ //блок без реинвестирования
+        if(dopdeposit == 0){//без реинвестирования без.доп вложений
         ProcentDeposit(&deposit, time, percent, &deposit1, &sumdeposit);
         
         printf("\nДоход процентами:%.1Lf", sumdeposit);
         printf("\nОбщий доход:%.1Lf", sumdeposit + deposit1);
-    }
-    
+        }
+        else{//без реинвестирования с доп.вложениями
+            SumProcentDeposit(&deposit, time, percent, &dopdeposit, &deposit1, &sumdeposit);
 
+            printf("\nДоход процентами:%.1Lf", sumdeposit);
+            printf("\nОбщий доход:%.1Lf", sumdeposit + deposit1);
+        }
+    }
     return 0;
 }
